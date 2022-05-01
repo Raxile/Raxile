2581768f59d134e2c8c605cbad90dfd823f2c0d3
Raxile <103770082+Raxile@users.noreply.github.com>
2022-05-01 23:47:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+require('./models/db');
+const port = process.env.PORT || 3000;
+
+const express = require ('express');
+const path = require('path');
+const exphbs =require('express-handlebars');
+const bodyparser = require('body-parser');
+
+
+const customerController = require('./controllers/customerController');
+const homeControler =require('./controllers/homeControler');
+
+var app = express();
+app.use(bodyparser.urlencoded({
+    extended:true
+}));
+app.use(bodyparser.json());
+
+app.set('views', path.join(__dirname,'/views/'));
+app.engine('hbs',exphbs.engine({ extname: 'hbs',defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layout/'}));
+app.set('view engine','hbs');
+
+
+app.listen(port, () => { 
+    console.log(`Express server started at port : ${port}`);
+});
+
+app.use('/',homeControler);
+
+app.use('/customer', customerController);
+
