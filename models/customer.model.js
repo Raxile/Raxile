bf614dae00a7ef03d0d1189921202dc6362bede2
Raxile <103770082+Raxile@users.noreply.github.com>
2022-05-02 00:02:09 +0530

    Update customer.model.js

--- models/customer.model.js
+++ models/customer.model.js
@@ -1,12 +1,29 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/CustomerDB' , {useNewUrlParser:true}, (err)=>{
-    if(!err){
-        console.log('MongoDB connection succeeded');
+var customerSchema = new mongoose.Schema({
+    firstname:{
+        type: String,
+        required: 'this field is required '
+    },
+    lastname:{
+        type: String,
+        required: 'this field is required' 
+    },
+    email:{
+        type: String
+    },
+    phoneNumber:{
+        type: String
+    },
+    password:{
+        type: String
     }
-    else {
-        console.log('Error in DB connection :'+ err);
-    }
-});
 
-require('./customer.model');
+});
+// Custom validation for email
+customerSchema.path('email').validate((val) => {
+    emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailRegex.test(val);
+}, 'Invalid e-mail.');
+
+mongoose.model('Customer', customerSchema);
